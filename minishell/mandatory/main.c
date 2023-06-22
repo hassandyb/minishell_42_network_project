46bb948c05de6224e3a2f93d05820bae794e934a
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-06-22 18:47:28 +0100

    Add files via upload

--- minishell/mandatory/main.c
+++ minishell/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 11:51:40 by hed-dyb           #+#    #+#             */
-/*   Updated: 2023/06/22 17:32:25 by hed-dyb          ###   ########.fr       */
+/*   Updated: 2023/06/22 18:46:50 by hed-dyb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,35 +45,6 @@ void ft_print_t_env_linked_list(t_env *e)//
 // expander
 // conncater
 // execution
-
-//---------------------------
-
-	// _white_space,  --
-	// _pipe,
-	// _append_output_re, //>>    --
-	// _here_document,//  << --- 
-	// _output_re,//  > -- 
-	// _input_re,//   < -- 
-	// _double_quote,//""
-	// _single_quote, //''
-	// _word,--
-//----------------------------
-
-
-
-// typedef struct s_env
-// {
-// 	char *var_name;
-// 	char *value;
-// 	struct s_env *next;
-// }t_env;
-
-//----------------
-//   env_line = "USER=hed-dyb";
-
-//   ABCD=EFGHI
-//   0123456789
-//   10 - 4 = 6
 
 
 char	*ft_substr(char *s, int start, int len, t_free *f)
@@ -148,55 +119,7 @@ void ft_add_back_t_env(t_env **e, t_env *node)
 // 	int j;
 // 	char *result;
 	
-// 	if(new_token == NULL)
-// 	{
-// 		new_token = malloc(1);
-// 		new_token[0]= '\0';
-// 	}
-// 	i = -1;
-// 	result = malloc((ft_strlen(new_token) + ft_strlen(to_add) + 1));
-// 	ft_protection(result, NULL, f);
-// 	while(new_token[++i])
-// 		result[i] = new_token[i];
-// 	j = 0;
-// 	while(to_add[j])
-// 	{
-// 		result[i] = to_add[j];
-// 		i++;
-// 		j++;
-// 	}
-// 	result[i] = '\0';
-// 	free(new_token);
-// 	return result;
-// }
 
-// void ft_expand_word(t_token *node, t_env *e, t_free *f)
-// {
-// 	int i =0;
-// 	// count ++;
-// 	char *new_token;
-// 	new_token = NULL;
-
-// }
-//-----------------------------
-//first char should be _ or alphabet up and lower
-/// sencend third  _ or alphabet up and lower or number
-
-
-void ft_replace(t_token *node, t_env *e, t_free *f)
-{
-	char *new_token;
-	int i;
-
-	i = 0;
-	while(node->token[i])
-	{
-		if(node->token[i] == '$' && (node->token[i + 1] == '_' || ft_is_alpha(node->token[i + 1]) == 1))
-		 //node->token[i + 1] &&  (node->token[i + 1] == '_' || ft_is_alpha )
-			//node->token = ft_  replace aaa$HOME+aa => aaa $HOME +aa ==>serach for home value ==>if it exit rplace home value =>>join all strings free.. free
-		i++;
-	}
-}
 void ft_expander(t_env **e, char **env, t_token **t, t_free *f)
 {
 	int i;
