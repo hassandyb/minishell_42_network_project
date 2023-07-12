7520a57123f61ba4ed211601e0be503988876b6c
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-07-12 18:05:07 +0100

    Add files via upload

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 11:51:40 by hed-dyb           #+#    #+#             */
-/*   Updated: 2023/07/11 22:49:54 by hed-dyb          ###   ########.fr       */
+/*   Updated: 2023/07/12 17:53:42 by hed-dyb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,21 @@ void ft_print_t_env_linked_list(t_env *e)//
 	printf("\n\n");
 }
 
+void ft_printf_double_ptr(char **str)
+{
+	if(str == NULL)
+	{
+		printf("NULL\n");
+		return ;
+	}
+	int i = 0;
+	while(str[i])
+	{
+		printf("%s  - ", str[i]);
+		i++;
+	}
+	printf("\n");
+}
 // tokenizer
 // parcer
 // expander
@@ -77,29 +92,31 @@ int ft_count(t_token *temp)
 
 char **ft_comand_with_args(t_token *temp, t_free *f)
 {
-	t_token *tp;
 	int count;
-	char *comand;
-	(void)f; //remove it later
+	char **comand;
 	count = ft_count(temp);
-	tp = temp;
+
 	
-	if (count = 0)
+	if (count == 0)
 		return NULL;
-	comand = malloc(count * sizeof(char *));
+	comand = malloc((count + 1) * sizeof(char *));
 	ft_protection_2(comand, NULL, f);
+	ft_add_t_free(&f, ft_create_t_free(comand, f));
 	count = 0;
 	while(temp && temp->type != _pipe)
 	{
-		if(ft_is_a_redirection(tp) == 1)
+		if(ft_is_a_redirection(temp) == 1)
 			temp = temp->next;
 		else
-			comand[count] = temp->token;
+		{
+			comand[count] = ft_strdup(temp->token, f);
+			count++;
+		}
 		temp = temp->next;
-		count++;
+		
 	}
-	printf("==>  %d\n", count);
-	return NULL;
+	comand[count] = NULL;
+	return (comand);
 }
 
 void ft_comand_info(t_cmd **cmd, t_token **t, t_free *f)
@@ -113,10 +130,10 @@ void ft_comand_info(t_cmd **cmd, t_token **t, t_free *f)
 	while(temp)
 	{
 		comand = ft_comand_with_args(temp, f);
-		break;
+		
 		// ft_fd_out_in(&fd_in, &fd_out, temp, f);
 		// ft_add_t_cmd(&cmd, ft_create_t_cmd(fd_in, fd_out, comand, f));
-		// temp = ft_next_part(temp);
+		temp = ft_next_part(temp);
 	}
 }
 
@@ -148,7 +165,7 @@ int main (int argc, char **argv, char  **env)
 		}
 		free(command);
 		ft_expander(&t, &e, env, f);
-		ft_print_t_token_linked_list(t);
+		// ft_print_t_token_linked_list(t);
 		ft_comand_info(&cmd, &t, f);
 	}
 	ft_free_all(f);	
